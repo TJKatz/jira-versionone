7e8206264117bf2d7413839cc441a20a7fad59fe
Tyler Katz <tyler.katz@mindtree.com>
2016-05-05 20:40:15 -0400

    commit all code

---
+++ JIRA_to_V1_Tool/MainForm.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace JIRA_to_V1_Tool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_fileSelected = new System.Windows.Forms.Label();
+            this.label_inputFile = new System.Windows.Forms.Label();
+            this.button_convert = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox_Rules = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Excel Workbook|*.xls|All files|*.*";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(234, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 105);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse for JIRA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(95, 105);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_fileSelected
+            // 
+            this.label_fileSelected.AutoSize = true;
+            this.label_fileSelected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_fileSelected.Font = new System.Drawing.Font("Microsoft NeoGothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_fileSelected.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.label_fileSelected.Location = new System.Drawing.Point(117, 109);
+            this.label_fileSelected.Name = "label_fileSelected";
+            this.label_fileSelected.Size = new System.Drawing.Size(120, 23);
+            this.label_fileSelected.TabIndex = 3;
+            this.label_fileSelected.Text = "No file selected";
+            // 
+            // label_inputFile
+            // 
+            this.label_inputFile.AutoSize = true;
+            this.label_inputFile.Font = new System.Drawing.Font("Microsoft NeoGothic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_inputFile.Location = new System.Drawing.Point(-2, 110);
+            this.label_inputFile.Name = "label_inputFile";
+            this.label_inputFile.Size = new System.Drawing.Size(119, 21);
+            this.label_inputFile.TabIndex = 4;
+            this.label_inputFile.Text = "JIRA File Name:";
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(344, 2);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(104, 105);
+            this.button_convert.TabIndex = 5;
+            this.button_convert.Text = "CONVERT to V1";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Visible = false;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "xls";
+            this.saveFileDialog1.Filter = "Excel Workbook|*.xls|All files|*.*";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // textBox_Rules
+            // 
+            this.textBox_Rules.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.textBox_Rules.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Rules.ForeColor = System.Drawing.Color.Black;
+            this.textBox_Rules.Location = new System.Drawing.Point(102, 12);
+            this.textBox_Rules.Multiline = true;
+            this.textBox_Rules.Name = "textBox_Rules";
+            this.textBox_Rules.ReadOnly = true;
+            this.textBox_Rules.Size = new System.Drawing.Size(128, 81);
+            this.textBox_Rules.TabIndex = 6;
+            this.textBox_Rules.TabStop = false;
+            this.textBox_Rules.Text = "JIRA \'Project\' and V1 \'Project\' must match EXACTLY";
+            this.textBox_Rules.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(452, 152);
+            this.Controls.Add(this.textBox_Rules);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.label_inputFile);
+            this.Controls.Add(this.label_fileSelected);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft NeoGothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JIRA to VersionOne Tool";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_fileSelected;
+        private System.Windows.Forms.Label label_inputFile;
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox_Rules;
+    }
+}
+
---
+++ JIRA_to_V1_Tool/MainForm.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace JIRA_to_V1_Tool
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            label_fileSelected.Text = openFileDialog1.FileName.Substring(openFileDialog1.FileName.LastIndexOf('\\') + 1);
+            button_convert.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void button_convert_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            //Start new excel application
+            Excel.Application excelApp = new Excel.Application();
+            
+            //Open JIRA output as 'jiraBook'
+            Excel.Workbook jiraBook = excelApp.Workbooks.Open(openFileDialog1.FileName);
+            
+            //Set sheets of jiraBook as 'jiraSheetSet'
+            Excel.Sheets jiraSheetSet = jiraBook.Worksheets;
+
+            //Set default number of sheets in new workbook to 7
+            //excelApp.SheetsInNewWorkbook = 7;
+
+            //Create new workbook 'v1Book' and save as chosen file dir/name            
+            //Excel.Workbook v1Book = excelApp.Workbooks.Add(Environment.CurrentDirectory + "\\Advanced_Backlog_Import_Template.xls");
+            Excel.Workbook v1Book = excelApp.Workbooks.Open(Environment.CurrentDirectory + "\\Advanced_Backlog_Import_Template.xls");
+            v1Book.SaveAs(saveFileDialog1.FileName);
+
+            //Set sheets of v1Book as 'v1SheetsSet'
+            Excel.Sheets v1SheetSet = v1Book.Worksheets;
+            Excel.Worksheet jiraSheet;
+            
+            //Rename sheets for template
+            
+            //Excel.Worksheet epicSheet;
+            Excel.Worksheet storySheet;
+            Excel.Worksheet taskSheet;
+            //Excel.Worksheet testSheet;
+            //Excel.Worksheet defectSheet;
+            //Excel.Worksheet requestSheet;
+            //Excel.Worksheet issueSheet;
+            
+            
+            
+            //epicSheet = (Excel.Worksheet)v1SheetSet.get_Item(1);
+            //epicSheet.Name = "1 - Epics";
+            storySheet = (Excel.Worksheet)v1SheetSet.get_Item(2);
+            //storySheet.Name = "2 - Stories";
+            taskSheet = (Excel.Worksheet)v1SheetSet.get_Item(3);
+            //taskSheet.Name = "3 - Tasks";
+            //testSheet = (Excel.Worksheet)v1SheetSet.get_Item(4);
+            //testSheet.Name = "4 - Tests";
+            //defectSheet = (Excel.Worksheet)v1SheetSet.get_Item(5);
+            //defectSheet.Name = "5 - Defects";
+            //requestSheet = (Excel.Worksheet)v1SheetSet.get_Item(6);
+            //requestSheet.Name = "6 - Requests";
+            //issueSheet = (Excel.Worksheet)v1SheetSet.get_Item(7);
+            //issueSheet.Name = "7 - Issues";
+            
+
+
+            jiraSheet = jiraSheetSet.get_Item(1);
+            
+            //Create column headers to complete v1 import template
+            String[] epicColumnHeaders = {"AssetType", "Name", "Scope", "Description", "Swag", "Reference", "Status", "Priority", "Risk", "Source", "Category", "Owner", "Super"};
+            String[] storyColumnHeaders = {"AssetType", "Name", "Scope", "Description",	"Estimate",	"OriginalEstimate",	"Reference", "RequestedBy",	"LastVersion", "Timebox", "Customer", "Status", "Priority", "Risk", "Source", "Category", "Parent", "Team",	"Owners", "Super"};
+            String[] taskColumnHeaders = {"AssetType", "Name", "Parent", "Description", "DetailEstimate", "ToDo", "Reference", "LastVersion", "Status", "Source", "Category", "Owners"};
+            String[] testColumnHeaders = {"AssetType", "Name", "Parent", "Description", "DetailEstimate", "ToDo", "Reference", "Setup", "Inputs", "Steps", "ExpectedResults", "ActualResults", "VersionTested", "Category", "Status", "Owners"};
+            String[] defectColumnHeaders = {"AssetType", "Name", "Scope", "Description", "Estimate", "DetailEstimate", "Reference", "Environment", "FoundBy", "FoundInBuild", "FixedInBuild", "VerifiedBy", "VersionAffected", "Timebox", "ToDo", "Status", "Priority", "ResolutionReason", "Resolution", "Source", "Type", "Parent", "Team", "Owners"};
+            String[] requestColumnHeaders = { "AssetType", "Name", "Scope", "Description", "Owner", "Category", "Priority", "Reference", "RequestedBy", "ResolutionReason", "Resolution", "Source", "Status"};
+            String[] issueColumnHeaders = {"AssetType", "Name", "Scope", "Description",	"Team", "Owner", "Category", "IdentifiedBy", "Priority", "Reference", "ResolutionReason", "Resolution", "Source", "TargetDate"};
+            Object[] columnHeaderArrays = { epicColumnHeaders, storyColumnHeaders, taskColumnHeaders, testColumnHeaders, defectColumnHeaders, requestColumnHeaders, issueColumnHeaders };
+            ArrayList columnHeaderArrayList = new ArrayList(columnHeaderArrays);
+
+            String assetType;
+            String numberOfIssues = jiraSheet.get_Range("A3").Value2;
+            int totalAssets = Int32.Parse(numberOfIssues.Substring(11,2));
+
+            //Copy range to object array 'jiraData' for data manipulation
+            Object[,] jiraData;
+            jiraData = jiraSheet.get_Range("A4", "FT" + (4 + totalAssets).ToString()).Value2;
+            
+            //Mapping algorithm
+            Object[,] v1EpicData = new Object[totalAssets + 1, 13];
+            Object[,] v1StoryData = new Object[totalAssets + 1, 20];
+            Object[,] v1TaskData = new Object[totalAssets + 1, 12];
+            Object[,] v1TestData = new Object[totalAssets + 1, 16];
+            Object[,] v1DefectData = new Object[totalAssets + 1, 24];
+            Object[,] v1RequestData = new Object[totalAssets + 1, 13];
+            Object[,] v1IssueData = new Object[totalAssets + 1, 14];
+            int[] v1DataArrayLengths = { v1EpicData.Length, v1StoryData.Length, v1TaskData.Length, v1TestData.Length, v1DefectData.Length, v1RequestData.Length, v1IssueData.Length};
+
+            /*Copying column headers for all sheets to v1 arrays
+            for (int i = 0; i < v1DataArrayLengths.Max(); i++)
+            {
+                if (i < epicColumnHeaders.Length)
+                    v1EpicData[0, i] = epicColumnHeaders[i];
+                if (i < storyColumnHeaders.Length)
+                    v1StoryData[0, i] = storyColumnHeaders[i];
+                if (i < taskColumnHeaders.Length)
+                    v1TaskData[0, i] = taskColumnHeaders[i];
+                if (i < testColumnHeaders.Length)
+                    v1TestData[0, i] = testColumnHeaders[i];
+                if (i < defectColumnHeaders.Length)
+                    v1DefectData[0, i] = defectColumnHeaders[i];
+                if (i < requestColumnHeaders.Length)
+                    v1RequestData[0, i] = requestColumnHeaders[i];
+                if (i < issueColumnHeaders.Length)
+                    v1IssueData[0, i] = issueColumnHeaders[i];
+            }
+            */
+            
+            //for loop iterates through JIRA rows looking for User Stories first
+            int currentTaskRow = 0;
+            int currentStoryRow = 0;
+            Dictionary<String, String> parentKeyMap = new Dictionary<String, String>(0);
+            
+            for(int jiraRow = 1; jiraRow < totalAssets + 2; jiraRow++){
+                assetType = (String)jiraData[jiraRow, 4];
+                if (assetType == "User Story")
+                {
+                    //Create story name (sub-task 'parent')
+                    String storyName = (String)jiraData[jiraRow, 2] + " " + (String)jiraData[jiraRow, 3];
+                    v1StoryData[currentStoryRow, 1] = storyName;
+                    String[] subTaskKeys;
+
+                    //Find sub-tasks if any and add to dictionary (JIRA Sub-task key, storyName)
+                    if (jiraData[jiraRow, 24] != null)
+                    {
+                        
+                        subTaskKeys = jiraData[jiraRow, 24].ToString().Split(',');
+                        for (int i = 0; i < subTaskKeys.Length; i++)
+                        {
+                            parentKeyMap.Add(subTaskKeys[i].Trim(), storyName);
+                        }
+                    }
+                    
+                    v1StoryData[currentStoryRow, 0] = "Story";
+                    v1StoryData[currentStoryRow, 2] = (String)jiraData[jiraRow, 1];
+
+                    String tempCOA = jiraData[jiraRow, 121] != null ? jiraData[jiraRow, 121].ToString() : "";
+                    String tempNotes = jiraData[jiraRow, 106] != null ? jiraData[jiraRow, 106].ToString() : "";
+
+                    v1StoryData[currentStoryRow, 3] = tempCOA + "\n\nNOTES: " + tempNotes;
+                    v1StoryData[currentStoryRow, 4] = jiraData[jiraRow, 148];
+                    v1StoryData[currentStoryRow, 5] = jiraData[jiraRow, 20] != null ? (Double.Parse(jiraData[jiraRow, 20].ToString())/3600).ToString() : "0";
+                    //v1StoryData[currentStoryRow, 9] = (String)jiraData[jiraRow, 132];
+                    
+                    /*Customer field (currently left blank)
+                    v1StoryData[currentStoryRow, 10] = (String)jiraData[jiraRow, 9];
+                    */
+                        //Status conversion
+                        String jiraStatus = (String)jiraData[jiraRow, 5];
+                        String v1Status;
+                        
+                        switch (jiraStatus)
+                        {
+                            case "Open":
+                                v1Status = "Ready";
+                                break;
+                            case "Closed":
+                                v1Status = "Done";
+                                break;
+                            default:
+                                v1Status = "In Progress";
+                                break;
+                        }
+                        v1StoryData[currentStoryRow, 11] = v1Status;
+                        
+                        //Priority conversion
+                        //String jiraPriority = (String)jiraData[jiraRow, 6];
+                        String v1Priority = "Medium";
+                        
+                        /*
+                        switch (jiraPriority)
+                        {
+                            case "Low (Low)":
+                                v1Priority = "Low";
+                                break;
+                            case "Minor (Medium)":
+                                v1Priority = "Medium";
+                                break;
+                            case "High (High)":
+                                v1Priority = "High";
+                                break;
+                            default:
+                                v1Priority = "Medium";
+                                break;
+                        }
+                        */
+                        
+                        //
+                        v1StoryData[currentStoryRow, 12] = v1Priority;
+                        //v1StoryData[currentStoryRow, 18] = jiraData[jiraRow, 8];
+                        currentStoryRow++;
+
+                }
+            }
+            
+            //Iterate through JIRA rows looking for sub-tasks
+            for(int jiraRow = 1; jiraRow < totalAssets + 2; jiraRow++){
+                assetType = (String)jiraData[jiraRow, 4];
+                if (assetType == "Sub-task"){
+                    v1TaskData[currentTaskRow, 0] = "Task";
+                    v1TaskData[currentTaskRow, 1] = (String)jiraData[jiraRow, 2] + ": " + (String)jiraData[jiraRow, 3];
+                    v1TaskData[currentTaskRow, 2] = parentKeyMap[jiraData[jiraRow, 2].ToString()];
+
+                    String tempTimeSpent = jiraData[jiraRow, 22] != null ? (Double.Parse(jiraData[jiraRow, 22].ToString()) / 3600).ToString() : "0";
+                    String tempNotes = jiraData[jiraRow, 106] != null ? jiraData[jiraRow, 106].ToString() : "None";
+                    
+                    v1TaskData[currentTaskRow, 3] = "Time Spent: " + tempTimeSpent + "\n\nNotes: " + tempNotes;
+                    v1TaskData[currentTaskRow, 4] = jiraData[jiraRow, 20] != null ? (Double.Parse(jiraData[jiraRow, 20].ToString()) / 3600).ToString() : "0";
+                    v1TaskData[currentTaskRow, 5] = jiraData[jiraRow, 21] != null ? (Double.Parse(jiraData[jiraRow, 21].ToString()) / 3600).ToString() : "0";
+
+                    //Status conversion
+                    String jiraStatus = (String)jiraData[jiraRow, 5];
+                    String v1Status;
+
+                    switch (jiraStatus)
+                    {
+                        case "Open":
+                            v1Status = "Ready";
+                            break;
+                        case "Closed":
+                            v1Status = "Done";
+                            break;
+                        default:
+                            v1Status = "In Progress";
+                            break;
+                    }
+
+                    //v1TaskData[currentTaskRow, 8] = v1Status;
+                    currentTaskRow++;
+                }
+            }
+            
+
+
+
+
+
+            //Object[,] v1Data = jiraData;
+
+            //Write to v1book (taskSheet & storySheet) and formatting
+            //epicSheet.get_Range("A1", "M1").Value2 = v1EpicData;
+            storySheet.get_Range("A2", "T" + (currentStoryRow + 1).ToString()).Value2 = v1StoryData;
+            taskSheet.get_Range("A2", "L" + (currentTaskRow + 1).ToString()).Value2 = v1TaskData;
+            //testSheet.get_Range("A1", "P1").Value2 = v1TestData;
+            //defectSheet.get_Range("A1", "X1").Value2 = v1DefectData;
+            //requestSheet.get_Range("A1", "M1").Value2 = v1RequestData;
+            //issueSheet.get_Range("A1", "N1").Value2 = v1IssueData;
+            
+            storySheet.get_Range("A1", "T" + (currentStoryRow + 1).ToString()).ColumnWidth = 33;
+            storySheet.get_Range("A1", "T" + (currentStoryRow + 1).ToString()).RowHeight = 30;
+            taskSheet.get_Range("A1", "L" + (currentTaskRow + 1).ToString()).ColumnWidth = 33;
+            taskSheet.get_Range("A1", "L" + (currentTaskRow + 1).ToString()).RowHeight = 30;
+
+            //Save and close
+            v1Book.Save();
+            excelApp.Application.Workbooks.Close();
+
+            //Notification
+            System.Windows.Forms.MessageBox.Show("Successful Launch!");
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
